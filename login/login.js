2383815cc9a8cf37a5a3e2a76277932c9c4aa748
Ryan Lau <rclau@usc.edu>
2023-03-30 12:02:07 -0700

    signup page js

--- login/login.js
+++ login/login.js
@@ -7,11 +7,12 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "12345") {
-        //alert("You have successfully logged in.");
-        //location.reload();
+    // search database for key value pair
+    const matchFound = Boolean(username === "user" && password === "12345"); // REPLACE
+    if (matchFound) {
         location = "../homepage/index.html"
     } else {
         loginErrorMsg.style.opacity = 1;
+        loginErrorMsg.textContent = "Invalid username and/or password";
     }
 })
---
+++ signup/signup.js
@@ -0,0 +1,24 @@
+const signupForm = document.getElementById("signup-form");
+const signupButton = document.getElementById("signup-form-submit");
+const signupErrorMsg = document.getElementById("signup-error-msg");
+
+signupButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = signupForm.username.value;
+    const password = signupForm.password.value;
+
+    if (username === "" || password === "") {
+        signupErrorMsg.style.opacity = 1;
+        signupErrorMsg.textContent = "Missing username and/or password";
+    } 
+    /*
+    else if (username already exists)  {
+        signupErrorMsg.style.opacity = 1;
+        signupErrorMsg.textContent = "Username already in use";
+    }
+    */
+    else {
+        location = "../homepage/index.html"
+    }
+    // add username and password to database
+})
